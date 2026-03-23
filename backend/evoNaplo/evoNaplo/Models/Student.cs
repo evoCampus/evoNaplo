2446d4739f9662dda131dc8d2004ceaf50280675
MilanGyulai <milan.gyulai@student.uni-miskolc.hu>
2026-03-23 10:59:04 +0100

    feat: created /Models and Mentor,Project,Student,Team.cs feat: created /Data and AppDbContext.cs remove: boilerplate codes

---
+++ backend/evoNaplo/evoNaplo/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿namespace evoNaplo.Models
+{ 
+public class Student
+    {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
+    public string? UniversityProgramme { get; set; }
+    public int? CurrentSemester { get; set; }
+    public bool IsFirstEvoCampusSemester { get; set; }
+    public string? PersonalGoals { get; set; }
+    public bool HasAppliedForScholarship { get; set; }
+    public bool HasActiveScholarship { get; set; }
+    public string? ScholarshipDuration { get; set; }
+    public bool HasAppliedForInternship { get; set; }
+    public bool IsCurrentlyIntern { get; set; }
+    public bool IsWorkingStudent { get; set; }
+    public string? WorkingStudentDuration { get; set; }
+    public bool WantsToStayWithCurrentTeam { get; set; }
+    public int? TeamId { get; set; }
+    public Team? Team { get; set; }
+    }
+}
---
+++ backend/evoNaplo/evoNaplo/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace evoNaplo.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? WeeklyMeetingTime { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; } = null!;
+        public ICollection<Mentor> Mentors { get; set; } = new List<Mentor>();
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
