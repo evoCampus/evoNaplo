b73487ec24ca6874fa4a3c3e49005e5f59d7f76e
Valentin Vaszil <vaszil.valentin03@gmail.com>
2026-03-25 21:31:24 +0100

    added more routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,43 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
+import AuthPage from "./pages/AuthPage";
+
 import AppLayout from "./components/layouts/AppLayout";
 import MentorLayout from "./components/layouts/MentorLayout";
 import AdminLayout from "./components/layouts/AdminLayout";
+
+import MentorHomePage from "./pages/mentor/MentorHomePage";
 import UpcomingMeetingsPage from "./pages/mentor/UpcomingMeetingsPage";
 import ProjectDetailsPage from "./pages/mentor/ProjectDetailsPage";
-import SemestersPage from "./pages/SemestersPage";
-import SettingsPage from "./pages/SettingsPage";
+
 import StudentsPage from "./pages/admin/StudentsPage";
 import MentorsPage from "./pages/admin/MentorsPage";
 import ProjectsPage from "./pages/admin/ProjectsPage";
-import QuickActionsPage from "./pages/admin/QuickActionsPage";
+import AdminHomePage from "./pages/admin/AdminHomePage";
+import TeamsPage from "./pages/admin/TeamsPage";
+
+import SemestersPage from "./pages/SemestersPage";
+import SettingsPage from "./pages/SettingsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { Navigate } from "react-router";
+
 
 function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
+        <Route index element={<AuthPage />} />
         <Route path="mentor" element={<MentorLayout />}>
-          <Route index element={<Navigate to="meetings" />} />
+          <Route index element={<MentorHomePage />} />
           <Route path="meetings" element={<UpcomingMeetingsPage />} />
           <Route path="projects/:id" element={<ProjectDetailsPage />} />
           <Route path="semesters" element={<SemestersPage />} />
           <Route path="settings" element={<SettingsPage />} />
         </Route>
         <Route path="admin" element={<AdminLayout />}>
-          <Route index path="" element={<QuickActionsPage />} />
+          <Route index element={<AdminHomePage />} />
           <Route path="students" element={<StudentsPage />} />
           <Route path="mentors" element={<MentorsPage />} />
+          <Route path="teams" element={<TeamsPage />} />
           <Route path="projects" element={<ProjectsPage />} />
           <Route path="semesters" element={<SemestersPage />} />
           <Route path="settings" element={<SettingsPage />} />
--- frontend/src/pages/admin/QuickActionsPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function QuickActionsPage() {
-  return <>Quick Actions</>;
-}
