4f603d36aee17603568a0e6f344d89bafe927b21
ruttkayzoltan <ruttkayzoltan06@gmail.com>
2026-04-28 18:48:51 +0200

    In DTOs: renamed Mentors, Students, Teams, Projects to MentorIds, TeamIds, etc..

--- backend/evoNaplo/evoNaplo/DTO/MentorDTO.cs
+++ backend/evoNaplo/evoNaplo/DTO/MentorDTO.cs
@@ -7,8 +7,8 @@ public class MentorDTO
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
     public string? MentorProfile { get; set; }
-    public IEnumerable<string>? Teams { get; set; }
-    public IEnumerable<string>? Projects { get; set; }
+    public IEnumerable<string>? TeamIds { get; set; }
+    public IEnumerable<string>? ProjectIds { get; set; }
     public int SemesterNumber { get; set; }
     public bool IsActive { get; set; }
 
