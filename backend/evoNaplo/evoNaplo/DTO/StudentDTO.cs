9d16bb618ca9025d36764c30fcad57c94ef7d2a3
ruttkayzoltan <ruttkayzoltan06@gmail.com>
2026-04-28 20:40:15 +0200

    Corrected controllers, adjusted DTOs

--- backend/evoNaplo/evoNaplo/DTO/StudentDTO.cs
+++ backend/evoNaplo/evoNaplo/DTO/StudentDTO.cs
@@ -7,12 +7,12 @@ public class StudentDTO
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
     public string? UniversityProgramme { get; set; }
-    public int CurrentSemester { get; set; }
+    public int? CurrentSemester { get; set; }
     public bool IsInTheirFirstSemester { get; set; }
     public string? PersonalGoals { get; set; }
     public bool HasAppliedForScholarship { get; set; }
     public bool HasScholarship { get; set; }
-    public int ScholarshipDurationInSemesters { get; set; }
+    public DateTime ScholarshipDuration { get; set; }
     public bool HasAppliedForInternship { get; set; }
     public bool HasInternship { get; set; }
     public bool IsWorkingStudent { get; set; }
