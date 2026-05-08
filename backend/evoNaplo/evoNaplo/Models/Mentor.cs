97585cd5273dd5e4e9e754eb774b769e4192144e
ruttkayzoltan <ruttkayzoltan06@gmail.com>
2026-05-08 19:04:45 +0200

    Remove unused properties from Mentor model and ensure proper formatting in related models

--- backend/evoNaplo/evoNaplo/Models/Mentor.cs
+++ backend/evoNaplo/evoNaplo/Models/Mentor.cs
@@ -6,9 +6,6 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string? PhoneNumber { get; set; }
-        public int TeamCount { get; set; }
-        public int StudentCount { get; set; }
-        public int ProjectCount { get; set; }
         public ICollection<Team> Teams { get; set; }
         public ICollection<Project> Projects { get; set; }
     }
