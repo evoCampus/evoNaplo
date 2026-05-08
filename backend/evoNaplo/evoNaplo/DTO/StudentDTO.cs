2f3427994f2adc4829e258cd3080039c82dae0b1
ruttkayzoltan <ruttkayzoltan06@gmail.com>
2026-05-08 18:06:21 +0200

    Made suggested changes to DTOs

--- backend/evoNaplo/evoNaplo/DTO/StudentDTO.cs
+++ backend/evoNaplo/evoNaplo/DTO/StudentDTO.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
+using evoNaplo.Models;
+
 namespace evoNaplo.DTO;
 
 public class StudentDTO
@@ -6,17 +9,37 @@ public class StudentDTO
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
-    public string? UniversityProgramme { get; set; }
+    public required string UniversityProgramme { get; set; }
     public required int CurrentSemester { get; set; }
     public required bool IsInTheirFirstSemester { get; set; }
-    public string? PersonalGoals { get; set; }
+    public required string PersonalGoals { get; set; }
     public required bool HasAppliedForScholarship { get; set; }
     public required bool HasScholarship { get; set; }
     public required DateTime ScholarshipDuration { get; set; }
     public required bool HasAppliedForInternship { get; set; }
     public required bool HasInternship { get; set; }
     public required bool IsWorkingStudent { get; set; }
-    public required int WorkExperienceInSemesters { get; set; }
+    public required DateTime WorkExperienceInSemesters { get; set; }
     public bool WantsToStayWithCurrentTeam { get; set; }
 
+    [SetsRequiredMembers]
+    public StudentDTO(Student student)
+    {
+        Id = student.Id;
+        Name = student.Name;
+        Email = student.Email;
+        PhoneNumber = student.PhoneNumber;
+        UniversityProgramme = student.UniversityProgramme;
+        CurrentSemester = student.CurrentSemester;
+        IsInTheirFirstSemester = student.IsFirstEvoCampusSemester;
+        PersonalGoals = student.PersonalGoals;
+        HasAppliedForScholarship = student.HasAppliedForScholarship;
+        HasScholarship = student.HasActiveScholarship;
+        ScholarshipDuration = student.ScholarshipDuration;
+        HasAppliedForInternship = student.HasAppliedForInternship;
+        HasInternship = student.IsCurrentlyIntern;
+        IsWorkingStudent = student.IsWorkingStudent;
+        WorkExperienceInSemesters = student.WorkingStudentDuration;
+        WantsToStayWithCurrentTeam = student.WantsToStayWithCurrentTeam;
+    }
 }
--- backend/evoNaplo/evoNaplo/DTO/TeamDTO.cs
+++ backend/evoNaplo/evoNaplo/DTO/TeamDTO.cs
@@ -1,4 +1,7 @@
-﻿namespace evoNaplo.DTO;
+﻿using System.Diagnostics.CodeAnalysis;
+using evoNaplo.Models;
+
+namespace evoNaplo.DTO;
 
 public class TeamDTO
 {
@@ -9,4 +12,14 @@ public class TeamDTO
     public required TimeSpan WeeklyMeetingTime { get; set; }
     public required IEnumerable<string> AttendanceSheetIds { get; set; }
 
+    [SetsRequiredMembers]
+    public TeamDTO(Team team)
+    {
+        Id = team.Id;
+        MentorIds = team.Mentors.Select(m => m.Id);
+        StudentIds = team.Students.Select(s => s.Id);
+        //WeeklyMeetingDay = team.WeeklyMeetingDay;
+        //WeeklyMeetingTime = team.WeeklyMeetingTime;
+        AttendanceSheetIds = team.AttendanceSheets.Select(a => a.Id);
+    }
 }
