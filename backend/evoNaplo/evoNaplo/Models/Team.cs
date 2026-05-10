18b736f0a192f7b3e15662946fe14f1d533302dd
MilanGyulai <milan.gyulai@student.uni-miskolc.hu>
2026-05-10 14:09:09 +0200

    rename:auth.sh to 100-setup-basic-aut.sh

--- backend/evoNaplo/evoNaplo/Models/Team.cs
+++ backend/evoNaplo/evoNaplo/Models/Team.cs
@@ -3,7 +3,7 @@
     public class Team
     {
         public string Id { get; set; }
-        public string  ProjectId { get; set; }
+        public string ProjectId { get; set; }
         public Project Project { get; set; }
         public ICollection<AttendanceSheet> AttendanceSheets { get; set; }
         public ICollection<Mentor> Mentors { get; set; }
