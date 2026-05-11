c94d6631486d20f6618a39a1ed2e5ccfa9c6f075
Valentin Vaszil <vaszil.valentin03@gmail.com>
2026-05-11 15:27:21 +0200

    Add ApiClient provider and enable CORS

--- backend/evoNaplo/evoNaplo/Program.cs
+++ backend/evoNaplo/evoNaplo/Program.cs
@@ -9,6 +9,19 @@ builder.Services.AddDatabaseServices(builder.Configuration);
 
 builder.Services.AddControllers();
 
+var allowedOrigins = builder.Configuration
+    .GetSection("AllowedOrigins")
+    .Get<string[]>() ?? [];
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,6 +40,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseAuthorization();
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import { ApiClientProvider } from "./components/ApiClientProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApiClientProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApiClientProvider>
   </StrictMode>,
 );
