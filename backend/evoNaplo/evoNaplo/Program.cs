a939c4e9c3c3a7b7f3198ef67fd0adc7298a8558
palb <bencehu2@gmail.com>
2026-05-20 04:23:20 +0200

    removed spelling mistake

--- backend/evoNaplo/evoNaplo/Program.cs
+++ backend/evoNaplo/evoNaplo/Program.cs
@@ -1,6 +1,6 @@
 using evoNaplo.Extensions;
 
-seavar builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices();
