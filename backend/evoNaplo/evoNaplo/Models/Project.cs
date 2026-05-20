023e91ddc16d9ccda02d12c0bb11444f20ad74af
MilanGyulai <149778992+MilanGyulai@users.noreply.github.com>
2026-05-20 16:13:10 +0200

    Added Nanoid Nuget (#18)

--- backend/evoNaplo/evoNaplo/Models/Project.cs
+++ backend/evoNaplo/evoNaplo/Models/Project.cs
@@ -1,8 +1,10 @@
-﻿namespace evoNaplo.Models
+﻿using NanoidDotNet;
+
+namespace evoNaplo.Models
 {
     public class Project
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Nanoid.Generate();
         public string Name { get; set; }
         public string? ShortDescription { get; set; }
         public ICollection<ProjectLink> ProjectLinks { get; set; }
