1ced567b39e3e79ba5da2ac2c09af9fe02ddc1c6
Stark Ádám <73966365+adamstark1@users.noreply.github.com>
2026-05-20 16:19:04 +0200

    Importing and exporting .xlsx and .csv files (#10)
    Co-authored-by: BenceKercsi <78799553+KercsiBence@users.noreply.github.com>

---
+++ backend/evoNaplo/evoNaplo/Services/Common/ExportHeaders.cs
@@ -0,0 +1,14 @@
+namespace evoNaplo.Services.Common;
+
+public static class ExportHeaders
+{
+    public const string HeaderTimestamp = "Időbélyeg";
+    public const string HeaderName = "Név";
+    public const string HeaderEmail = "E-mail cím";
+    public const string HeaderPhoneNumber = "Telefonszám (pl: 70/1234567)";
+    public const string HeaderMajor = "Tanulmányi évfolyam és szak megnevezése (pl.: Miskolci Egyetem Gépészmérnöki és Informatikai Kar ... szak ...specializáció, 3. félév)";
+    public const string HeaderIsFirstTime = "Először veszel részt az evoCampus programban?";
+    public const string HeaderGoals = "Milyen személyes céljaid vannak az evoCampus 2026 tavaszi szemeszteri részvételével? + A jelenlegi csapatban maradnál?";
+    public const string HeaderStayInTeam = "A jelenlegi csapatban maradnál?";
+    public const string HeaderOtherComments = "Egyéb észrevétel, megjegyzés:";
+}
